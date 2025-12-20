cd1955e6f6b1d8c7f7545f8a7588ea5d8744465b
gabeust <romero.gabriel.e@hotmail.com>
2025-12-20 10:27:56 -0300

    feat: add Prediction entity

---
+++ src/main/java/com/flightOnTime/flightOnTime/entity/Prediction.java
@@ -0,0 +1,27 @@
+package com.flightOnTime.flightOnTime.entity;
+
+import com.flightOnTime.flightOnTime.enums.PredictionStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "predictios")
+public class Prediction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private PredictionStatus prevision;
+    private Double probabilidad;
+    private LocalDateTime fechaPrediccion;
+
+}
