d9347ef7389fa7882b2439139b31236f25acbb77
gabeust <romero.gabriel.e@hotmail.com>
2025-12-20 11:24:25 -0300

    feat: add PredictionRepository

--- src/main/java/com/flightOnTime/flightOnTime/repository/FlightRequestRepository.java
+++ src/main/java/com/flightOnTime/flightOnTime/repository/FlightRequestRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FlightRequestRespository extends JpaRepository<FlightRequest, Long> {
+public interface FlightRequestRepository extends JpaRepository<FlightRequest, Long> {
 }
---
+++ src/main/java/com/flightOnTime/flightOnTime/repository/PredictionRepository.java
@@ -0,0 +1,10 @@
+package com.flightOnTime.flightOnTime.repository;
+
+import com.flightOnTime.flightOnTime.entity.Prediction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PredictionRepository extends JpaRepository<Prediction, Long> {
+
+}
