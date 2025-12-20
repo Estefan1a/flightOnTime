a3d306b049a534488fa2599bcbd687d30c4f409c
gabeust <romero.gabriel.e@hotmail.com>
2025-12-20 12:48:01 -0300

    feat: add OraclePredictionClient

---
+++ src/main/java/com/flightOnTime/flightOnTime/client/OraclePredictionClient.java
@@ -0,0 +1,41 @@
+package com.flightOnTime.flightOnTime.client;
+
+import com.flightOnTime.flightOnTime.dto.FlightRequestDTO;
+import com.flightOnTime.flightOnTime.dto.PredictionResponseDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+/**
+@Component
+@AllArgsConstructor
+public class OraclePredictionClient {
+    private final RestTemplate restTemplate;
+
+    @Value("${oracle.prediction.url}")
+    private String oracleUrl;
+
+    public PredictionResponseDTO predict(FlightRequestDTO request) {
+        return restTemplate.postForObject(
+                oracleUrl,
+                request,
+                PredictionResponseDTO.class
+        );
+    }
+    }
+*/
+    @Component
+    @Profile("mock-oracle")
+    public class OraclePredictionClient {
+
+        public PredictionResponseDTO predict(FlightRequestDTO request) {
+
+            return new PredictionResponseDTO(
+                    "RETRASADO",
+                    0.85
+            );
+        }
+    }
+
+
