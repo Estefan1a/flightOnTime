4d2dbe6c34b9fba5ff3a1693b92d795c37a8622e
Estefanía González <estefania.gonzre@gmail.com>
2026-01-02 13:14:08 -0600

    Agrega el cliente para DS

--- src/main/java/com/flightOnTime/flightOnTime/service/DataScienceClient.java
+++ src/main/java/com/flightOnTime/flightOnTime/service/DataScienceClient.java
@@ -1,4 +1,19 @@
 package com.flightOnTime.flightOnTime.service;
 
+import com.flightOnTime.flightOnTime.dto.PredictionRequestDTO;
+import com.flightOnTime.flightOnTime.dto.PredictionResponseDTO;
+import org.springframework.web.client.RestTemplate;
+
 public class DataScienceClient {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final String MODEL_URL = "http://localhost:8000/predict";
+
+    public PredictionResponseDTO predict(PredictionRequestDTO request) {
+        return restTemplate.postForObject(
+                MODEL_URL,
+                request,
+                PredictionResponseDTO.class
+        );
+    }
 }
