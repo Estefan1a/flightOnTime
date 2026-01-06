1073072d9944ea91ade58755bd40fc79429861f3
gabeust <romero.gabriel.e@hotmail.com>
2026-01-06 15:21:02 -0300

    feat(stats): add countByPrevision method to PredictionRepository

--- src/main/java/com/flightOnTime/flightOnTime/repository/PredictionRepository.java
+++ src/main/java/com/flightOnTime/flightOnTime/repository/PredictionRepository.java
@@ -1,10 +1,12 @@
 package com.flightOnTime.flightOnTime.repository;
 
 import com.flightOnTime.flightOnTime.entity.Prediction;
+import com.flightOnTime.flightOnTime.enums.PredictionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PredictionRepository extends JpaRepository<Prediction, Long> {
+    long countByPrevision(PredictionStatus prevision);
 
 }
